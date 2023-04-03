03b49688d45ab2e32d82abec1bfc63a9eb24dffe
Pascal Germain <pascalgermain@me.com>
2023-04-03 12:32:35 +0200

    add eslint-plugin-simple-import-sort

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,7 +2,7 @@ module.exports = {
 	root: true,
 	parser: '@typescript-eslint/parser',
 	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-	plugins: ['svelte3', '@typescript-eslint'],
+	plugins: ['svelte3', '@typescript-eslint', 'simple-import-sort'],
 	ignorePatterns: ['*.cjs'],
 	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
 	settings: {
@@ -16,5 +16,24 @@ module.exports = {
 		browser: true,
 		es2017: true,
 		node: true,
+	},
+	rules: {
+		quotes: ['warn', 'single', { avoidEscape: true, allowTemplateLiterals: false }],
+		'import/no-duplicates': 'off',
+		'import/order': 'off',
+		'simple-import-sort/imports': [
+			'warn',
+			{
+				// https://github.com/lydell/eslint-plugin-simple-import-sort#custom-grouping
+				// https://github.com/lydell/eslint-plugin-simple-import-sort/blob/main/examples/.eslintrc.js
+				groups: [
+					['^\\u0000'], // side effect imports
+					['^@sveltejs', '^svelte', '^@?\\w'], // starting with @sveltejs, with svelte, other "external" modules
+					['^(?!\\$lib/components)'], // NOT starting with $lib/components
+					['^', '^\\.'], // the rest, relative imports (starting wit a dot)
+				],
+			},
+		],
+		'simple-import-sort/exports': 'error',
 	},
 }
--- README.md
+++ README.md
@@ -5,16 +5,19 @@
 > http://pascalgermain.github.io/svelte-model-viewer/
 
 ## Project setup
+
 ```bash
 pnpm install
 ```
 
 ### Serves and hot-reloads for development
+
 ```bash
 pnpm run dev
 ```
 
 ### Builds as static site for production
+
 ```bash
 pnpm run build
 ```
